44fb7de4dc3e2a3f0bfcbe7525b68b0dc51d79c1
Nishant Kumar <nishantkumar19041@gmail.com>
2023-01-24 22:44:04 +0530

    A song added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,12 @@ import Music from './components/UI/music';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
+      <h1 className="app-name"> TruBeat </h1>
+      <h1 className="app-caption"> Beats for everyone... </h1>
+      <div className='App'>
       <Music />
+      </div>
     </div>
   );
 }
--- my-app/src/components/UI/music.js
+++ my-app/src/components/UI/music.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./music.css";
-import song from "../audio/1.mp3";
+import song from "../audio/2.mp3";
+import { Play, Pause,  SpeakerHigh} from "phosphor-react";
 
 class App extends Component {
   state = {
@@ -15,21 +16,27 @@ class App extends Component {
       this.state.audio.pause();
     } else {
       this.state.audio.play();
-    }
+      }
+      
     this.setState({ isPlaying: !isPlaying });
   };
 
   render() {
     return (
-      <div>
-        <h1 className="app-name"> TruBeat </h1>
-        <button className="Button" onClick={this.playPause} id="play-icon">
-          Play | Pause
+        <div className="container">
+            <h1>Kala Chasma</h1>
+        <button className="Button button1" onClick={this.playPause}>
+                { !this.state.isPlaying ? <Play size={32} /> : <Pause size={32} /> }  
         </button>
         
         <p className="Status">
           { this.state.isPlaying ? "Playing..." : "Paused" }
         </p>
+        <div class="slidecontainer">        
+            <SpeakerHigh size={32} />
+            <input type="range" min="1" max="100" >
+            </input>
+        </div>
       </div>
     );
   }
